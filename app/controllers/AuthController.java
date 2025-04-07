99d4fa495ef77c5b93eea54f096ae76522a4e709
Pentodo <matheus.a.v.b@gmail.com>
2025-04-06 22:23:19 -0300

    feat: add access token persistence and logout route

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import services.AuthService;
+import views.html.auth;
 
 import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
@@ -18,14 +22,35 @@ public class AuthController extends Controller {
         this.ws = ws;
     }
 
-    public Result auth() {
-        return ok(views.html.auth.render(authService.getAuthorizationUrl()));
+    public Result auth(Http.Request request) {
+        final String accessToken = request.session().get("accessToken").orElse(null);
+        final String authorizationUrl = accessToken == null ? authService.getAuthorizationUrl() : null;
+
+        return ok(auth.render(authorizationUrl, accessToken));
     }
 
-    public CompletionStage<Result> callback(String code) {
+    public CompletionStage<Result> callback(String code, Http.Request request) {
         return ws.url(authService.getTokenUrl(code))
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
                 .post("")
-                .thenApply(response -> status(response.getStatus(), response.getBody()));
+                .thenApply(response -> {
+                    if (response.getStatus() != 200) {
+                        return status(response.getStatus(), response.getBody());
+                    }
+
+                    try {
+                        return redirect("/").addingToSession(
+                                request,
+                                "accessToken",
+                                Json.mapper().readTree(response.getBody()).get("access_token").asText()
+                        );
+                    } catch (JsonProcessingException e) {
+                        return status(500, e.getMessage());
+                    }
+                });
+    }
+
+    public Result logout(Http.Request request) {
+        return redirect("/").removingFromSession(request, "accessToken");
     }
 }
--- build.sbt
+++ build.sbt
@@ -9,5 +9,6 @@ scalaVersion := "2.13.16"
 
 libraryDependencies ++= Seq(
   guice,
-  javaWs
+  javaWs,
+  "org.playframework" %% "play-json" % "3.0.4"
 )
